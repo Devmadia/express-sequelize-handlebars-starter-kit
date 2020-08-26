fca5b2942cae0103f3d9764668a0fb66a1f93e51
xtophe xanon <xocon10@gmail.com>
2020-08-26 14:02:46 -0500

    update api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 // collect endpoints
-const randoRoutes = require('./rando-routes.js');
+// const randoRoutes = require('./rando-routes.js');
 
 
 // prefixed names
-router.use('/randos', randoRoutes);
+// router.use('/randos', randoRoutes);
 
 
 module.exports = router;
